6674eade7ff1f86ba6a2311d897130326c094c80
Maxime Damour <15254620+Damyyr@users.noreply.github.com>
2023-10-31 22:20:38 -0400

    Initial commit

---
+++ 1/1.py
@@ -0,0 +1,48 @@
+from array import *
+
+import inputs
+
+outcomes = dict(zip(['ascend', 'descend', 'same', 'null'], [0,0,0,0]))
+
+def compare(first, second): 
+    outcome = 'null'
+    
+    if first == second:
+        outcome = 'same'
+    elif second > first:
+        outcome = 'descend'
+    elif second < first:
+        outcome = 'ascend'
+        
+    outcomes[outcome] += 1
+    print('{0} - ({1})'.format(first, outcome))
+
+def processChallenge2(input):
+    result = []
+    for i, value in enumerate(input):
+        if i == 0: next
+        if i == 1: next
+        
+        result.append(input[i-2] + input[i - 1] + value)
+        
+    return result
+
+# --------------------------------------    
+# First challenge (answer: 1791)
+# chal1 = inputs.INPUTS
+# --------------------------------------
+# Second challenge (answer: 1822)
+chal2 = processChallenge2(inputs.INPUTS)
+# --------------------------------------
+
+inputToConsider = chal2
+
+# Compare each value with the previous one
+for i, value in enumerate(inputToConsider):
+    if i == 0: next
+    compare(inputToConsider[i - 1], value)
+
+
+# Print all outcomes. Be carefull: outcome values can be accessed globally
+for k, v in outcomes.items():
+    print(k, v)
---
+++ 2/1.py
@@ -0,0 +1,61 @@
+import inputs
+
+def multiplyAll(array):
+    r = 1
+    for value in array:
+        r = value * r
+        
+    return r
+
+# Answer 2102357 
+def chal1(instruction, value, results):   
+    match instruction:
+        case 'down':
+            results['depth'] += value
+        case 'up':
+            results['depth'] -= value
+        case 'forward':
+            results['horizontal'] += value
+        case _:
+            raise 'Error'
+        
+    return results
+
+# Answer 2101031224
+def chal2(instruction, value, results):
+    match instruction:
+        case 'down':
+            results['aim'] += value
+        case 'up':
+            results['aim'] -= value
+        case 'forward':
+            results['horizontal'] += value
+            results['depth'] += results['aim'] * value
+        case _:
+            raise 'Error'
+        
+    return results
+
+# ------------------------- Runtime
+
+
+def main(name, inputToConsider):
+    chal1Results = {'horizontal': 0, 'depth': 0}
+    chal2Results = {'horizontal': 0, 'depth': 0, 'aim': 0}
+
+    for (k,v) in inputToConsider:
+        # print('Instructions are: {0} -> {1}'.format(k, v)) # debug printing
+    
+        chal1Results = chal1(k, v, chal1Results)
+        chal2Results = chal2(k, v, chal2Results)
+    
+    print('Chalenge 1:')
+    print(chal1Results)
+    print('Results: {0}'.format(multiplyAll(chal1Results.values())))
+    print('-----')
+    print('Chalenge 2:')
+    print(chal2Results)
+    print('Results for {0} values: {1}'.format(name, multiplyAll([chal2Results['horizontal'], chal2Results['depth']])))
+
+main('test', inputs.TEST)
+main('real', inputs.INPUTS)
