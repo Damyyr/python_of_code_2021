31b49bed295602585813398d1089b48259dc6695
Maxime Damour <15254620+Damyyr@users.noreply.github.com>
2023-10-31 22:50:03 -0400

    Lint && Gitignore

--- 1/1.py
+++ 1/1.py
@@ -1,33 +1,34 @@
-from array import *
-
 import inputs
 
-outcomes = dict(zip(['ascend', 'descend', 'same', 'null'], [0,0,0,0]))
+outcomes = dict(zip(['ascend', 'descend', 'same', 'null'], [0, 0, 0, 0]))
 
-def compare(first, second): 
+
+def compare(first, second):
     outcome = 'null'
-    
+
     if first == second:
         outcome = 'same'
     elif second > first:
         outcome = 'descend'
     elif second < first:
         outcome = 'ascend'
-        
+
     outcomes[outcome] += 1
     print('{0} - ({1})'.format(first, outcome))
+
 
 def processChallenge2(input):
     result = []
     for i, value in enumerate(input):
-        if i == 0: next
-        if i == 1: next
-        
-        result.append(input[i-2] + input[i - 1] + value)
-        
+        if i == 0 or i == 1:
+            next
+
+        result.append(input[i - 2] + input[i - 1] + value)
+
     return result
 
-# --------------------------------------    
+
+# --------------------------------------
 # First challenge (answer: 1791)
 # chal1 = inputs.INPUTS
 # --------------------------------------
@@ -39,7 +40,8 @@ inputToConsider = chal2
 
 # Compare each value with the previous one
 for i, value in enumerate(inputToConsider):
-    if i == 0: next
+    if i == 0:
+        next
     compare(inputToConsider[i - 1], value)
 
 
--- 2/1.py
+++ 2/1.py
@@ -1,14 +1,16 @@
 import inputs
 
+
 def multiplyAll(array):
     r = 1
     for value in array:
         r = value * r
-        
+
     return r
 
-# Answer 2102357 
-def chal1(instruction, value, results):   
+
+# Answer 2102357
+def chal1(instruction, value, results):
     match instruction:
         case 'down':
             results['depth'] += value
@@ -18,8 +20,9 @@ def chal1(instruction, value, results):
             results['horizontal'] += value
         case _:
             raise 'Error'
-        
+
     return results
+
 
 # Answer 2101031224
 def chal2(instruction, value, results):
@@ -33,7 +36,7 @@ def chal2(instruction, value, results):
             results['depth'] += results['aim'] * value
         case _:
             raise 'Error'
-        
+
     return results
 
 # ------------------------- Runtime
@@ -43,19 +46,20 @@ def main(name, inputToConsider):
     chal1Results = {'horizontal': 0, 'depth': 0}
     chal2Results = {'horizontal': 0, 'depth': 0, 'aim': 0}
 
-    for (k,v) in inputToConsider:
+    for (k, v) in inputToConsider:
         # print('Instructions are: {0} -> {1}'.format(k, v)) # debug printing
-    
         chal1Results = chal1(k, v, chal1Results)
         chal2Results = chal2(k, v, chal2Results)
-    
+
     print('Chalenge 1:')
     print(chal1Results)
     print('Results: {0}'.format(multiplyAll(chal1Results.values())))
     print('-----')
     print('Chalenge 2:')
     print(chal2Results)
-    print('Results for {0} values: {1}'.format(name, multiplyAll([chal2Results['horizontal'], chal2Results['depth']])))
+    toMultiply = [chal2Results['horizontal'], chal2Results['depth']]
+    print('Results for {0} values: {1}'.format(name, multiplyAll(toMultiply)))
+
 
 main('test', inputs.TEST)
 main('real', inputs.INPUTS)
